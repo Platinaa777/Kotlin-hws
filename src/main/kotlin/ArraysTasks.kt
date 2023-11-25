f922d216c8ac415c138d8c9bdb1b945b3244caff
denis <miroshnichenkodenis2004@gmail.com>
2023-11-25 13:47:18 +0300

    done 1 task

--- src/main/kotlin/ArraysTasks.kt
+++ src/main/kotlin/ArraysTasks.kt
@@ -1,11 +1,21 @@
-
 /**
 Задание 1: Сформировать целочисленный массив A из N элементов
 при помощи ввода значений с клавиатуры (N вводит пользователь).
 Вывести массив на экран. (1 балл)
  */
 fun makeArray(): Array<Int> {
-    return arrayOf()
+    var list: Array<Int> = arrayOf()
+    print("Enter count of number in array: ")
+    var n: Int = readLine()!!.toInt()
+
+    for (i in 1..n step 1) {
+        var element = readLine()!!.toInt()
+        list += element
+    }
+
+    println(list.joinToString())
+
+    return list
 }
 
 /**
@@ -13,13 +23,12 @@ fun makeArray(): Array<Int> {
 Использовать встроенные функции. (0.5 балла)
  */
 fun sortArrayDesc(arr: Array<Int>) {
-
+    arr.sortDescending()
+    println(arr.joinToString(" "))
 }
 
-// Используйте эту функцию для запуска кода
-// Раскомментируйте нужные участки в процессе реализации
 fun main() {
-//    val arr = makeArray()
-//
-//    sortArrayDesc(arr)
+    val arr = makeArray()
+
+    sortArrayDesc(arr)
 }
